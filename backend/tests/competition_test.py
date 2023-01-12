985c23a1be501c6b27b2e6266f52a8bc5f55b839
Josh Ramos <josh.ramos@student.unsw.edu.au>
2023-01-12 15:23:39 +1100

    wrote test for competition details

--- backend/tests/competition_test.py
+++ backend/tests/competition_test.py
@@ -6,7 +6,7 @@ from src import config
 import json
 import requests
 
-####################################### TESTS FOR CHANNEL_LIST ####################################
+####################################### TESTS FOR COMP_LIST ####################################
 
 def test_no_comps(clear, register_user1):
     token = register_user1['token']
@@ -34,3 +34,32 @@ def test_non_member_cant_see_comp(clear, sample_comp1, register_user2):
     
     # as user 2 is not a member of the sample channel, it should not appear in their list
     assert len(competitions) == 0
+    
+
+####################################### TESTS FOR COMP_DETAILS ##################################
+def test_all_details_correct(clear, sample_comp1):
+    owner_tok = sample_comp1['owner_tok']
+    owner_id = sample_comp1['owner_id']
+    
+    comp_id = sample_comp1['comp_id']
+    
+    resp = requests.get(config.url + 'competition/details/v1', params = { 'token' : owner_tok, 'comp_id': comp_id})
+    assert resp.status_code == 200
+    
+    details = json.loads(resp.text)
+    
+    assert details['name'] == "Sample Competition"
+    assert details['is_active'] == True
+    assert details['owner'] == owner_id
+    assert details['max_points_per_game'] == 15
+
+def test_invalid_channel_id(clear, register_user1):
+    resp = requests.get(config.url + 'competition/details/v1', params = { 'token' : register_user1['token'], 'comp_id': -2312})
+    assert resp.status_code == 403
+
+def test_non_member_cant_access_details(clear, sample_comp1, register_user2):
+    comp_id = sample_comp1['comp_id']
+    intruder_tok = register_user2['token']
+    
+    resp = requests.get(config.url + 'competition/details/v1', params = { 'token' : intruder_tok, 'comp_id': comp_id})
+    assert resp.status_code == 403
